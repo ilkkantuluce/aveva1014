68af6e6fadddcdbd45893876d713d69e83a2226e
ilkkantuluce07 <60572381+ilkkantuluce07@users.noreply.github.com>
2021-07-16 17:39:53 +0300

    updates

--- assets/js/new-scenes.js
+++ assets/js/new-scenes.js
@@ -18,12 +18,38 @@ $(document).ready(function() {
     $('.industry').addClass('level');
     $('.legend').addClass('slide-in');
 
-    ///$('.industry .chemicals .col-left .hide-on-level').animate({top: '100px', opacity: '0'}, 'slow');
+    $('.industry.level-plant .chemicals .col-left .hide-on-level').animate({top: '100px', opacity: '0'}, 'slow');
+    $('.industry.level-enterprise .chemicals .col-right .hide-on-level').animate({top: '100px', opacity: '0'}, 'slow');
     //$('.industry .chemicals .col-left  .hide-on-level').animate({display: 'none'}, 'slow');
-
+    
     $('.industry .fade').fadeOut();
-  
 
+
+    $('.scenes-wrapper .title-wrapper.show-on-level div').css({'animation-name': 'show-on-level', 'animation-duration': '1s', 'opacity': '1'});
+
+
+    setTimeout(function() {
+      $('.nav.top-nav').css({'margin-bottom': '150px'});
+    }, 200);
+  
+    setTimeout(function() {
+      $('.box-wrapper').css('display', 'flex');
+      $('.col-wrapper .col').css('flex-basis', '100%');
+      $('.title-wrapper').css('flex-basis', '50%');
+      $('.hide-on-level').css('flex-basis', '50%');
+      $('.level.level-plant .col-enterprise').hide();
+      $('.level.level-enterprise .col-plant').hide();
+
+
+
+
+
+
+
+
+
+
+    }, 500);
 
     setTimeout(function() {
       $('.pulse-dot').addClass('show-dot');
@@ -34,6 +60,7 @@ $(document).ready(function() {
 
   $('.industry.level-plant .chemicals .col-left .hide-on-level').fadeOut();
   $('.industry.level-enterprise .chemicals .col-right .hide-on-level').fadeOut();
+   
 
 
 
